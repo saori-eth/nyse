3634c5bec49f9de0e0eaea2dfd0527df54052ba1
saori-eth <D84da@proton.me>
2024-09-16 14:22:06 -0500

    make jumping more reliable

--- components/game/controls/isGrounded.ts
+++ components/game/controls/isGrounded.ts
@@ -1,14 +1,28 @@
-import { type RapierContext } from "@react-three/rapier";
+import { type RapierRigidBody, type RapierContext } from "@react-three/rapier";
 
+const COOLDOWN = 500;
+let lastGroundedTime = 0;
 export const isGrounded = (
   rapierData: RapierContext,
-  translation: { x: number; y: number; z: number }
+  physics: RapierRigidBody
 ): boolean => {
+  if (Date.now() - lastGroundedTime < COOLDOWN) {
+    return false;
+  }
   const { world, rapier } = rapierData;
+  const translation = physics.translation();
   const ray = world.castRay(
     new rapier.Ray(translation, { x: 0, y: -1, z: 0 }),
-    Infinity,
-    false
+    0.5,
+    false,
+    undefined,
+    undefined,
+    undefined,
+    physics
   );
-  return ray != null && Math.abs(ray.timeOfImpact) <= 0.459002;
+  const grounded = ray != null && Math.abs(ray.timeOfImpact) <= 0.459002;
+  if (grounded) {
+    lastGroundedTime = Date.now();
+  }
+  return grounded;
 };
